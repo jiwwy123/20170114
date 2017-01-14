b01b552fa25a5107626e49e6b8754498f13dbbc1
jiwwy <jiwwy123@gmail.com>
2017-01-14 09:39:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/a20170114/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.student.a20170114;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    ProgressBar pb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
